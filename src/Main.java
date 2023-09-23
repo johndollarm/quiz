e1ca85eb58b274578b62b7d163f300f4815311bc
johndollarm <johndollar1996@gmail.com>
2023-09-24 00:29:47 +0530

    added command

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
+    //main function
     public static void main(String[] args) {
         QuestionService service=new QuestionService();
         service.playQuiz();
